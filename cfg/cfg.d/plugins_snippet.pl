c3dc9b0ecec891e2a5c9fabacf8b0335eb5ac161
mpbraendle <marbra@idmacmb.uzh.ch>
2017-02-13 17:34:00 +0100

    Initial commit

---
+++ cfg/cfg.d/plugins_snippet.pl
@@ -0,0 +1,5 @@
+# Change for Batch Edit
+$c->{plugin_alias_map}->{"Export::BatchEdit"} = "Export::ZoraBatchEdit";
+$c->{plugin_alias_map}->{"Export::ZoraBatchEdit"} = undef;
+# END Change for Batch Edit
+
---
+++ cfg/static/javascript/auto/zora_screen_batchedit.js
@@ -0,0 +1,26 @@
+//
+// Function to show/hide the search and replace fields for batch edit
+//
+
+function EPJS_BatchEditActionSelect( selectPrefix, divPrefix )
+{
+        if (null != selectPrefix && null != divPrefix ) {
+		var selectName =  selectPrefix+"_action";
+		var selectObj = $(selectName);
+		var divObj = $(divPrefix);
+		if ( null != selectObj && null != divObj ) {
+			var selectedIndex = selectObj.selectedIndex;
+			var selectedVal = selectObj.options[selectedIndex].value;
+			if ( null != selectedVal && selectedVal == "findreplace" ) {
+				divObj.setStyle({visibility: 'visible'});
+				divObj.setStyle({display: 'block'});
+			} else {
+				divObj.setStyle({visibility: 'hidden'});
+				divObj.setStyle({display: 'none'});
+			}
+		}
+        }
+        return false;
+}
+
+
