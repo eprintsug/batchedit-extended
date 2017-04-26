240073fd76ef162421a3dc5735fffae77fd38f30
Rory McNicholl <rory.mcnicholl@ulcc.ac.uk>
2017-04-27 00:42:54 +0100

    forgot the Zora

--- cfg/cfg.d/z_batchedit_extended.pl
+++ cfg/cfg.d/z_batchedit_extended.pl
@@ -1,6 +1,6 @@
 # Change for Batch Edit
-$c->{plugins}{"Export::BatchEdit"}{params}{disable} = 0;
-$c->{plugins}{"Screen::BatchEdit"}{params}{disable} = 0;
+$c->{plugins}{"Export::ZoraBatchEdit"}{params}{disable} = 0;
+$c->{plugins}{"Screen::ZoraBatchEdit"}{params}{disable} = 0;
 
 $c->{plugin_alias_map}->{"Export::BatchEdit"} = "Export::ZoraBatchEdit";
 $c->{plugin_alias_map}->{"Export::ZoraBatchEdit"} = undef;
